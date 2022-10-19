d81979d4b4d72903e68eaa7ded9f8aecd37ef0fc
Ning Sun <sunng@protonmail.com>
2022-10-19 21:24:44 +0800

    refactor: merge CustomWithArgs into Custom

--- src/parser.rs
+++ src/parser.rs
@@ -394,7 +394,7 @@ impl<'a> Parser<'a> {
                 // name, resulting in `NOT 'a'` being recognized as a `TypedString` instead of
                 // an unary negation `NOT ('a' LIKE 'b')`. To solve this, we don't accept the
                 // `type 'string'` syntax for the custom data types at all.
-                DataType::Custom(..) | DataType::CustomWithArgs(..) => parser_err!("dummy"),
+                DataType::Custom(..) => parser_err!("dummy"),
                 data_type => Ok(Expr::TypedString {
                     data_type,
                     value: parser.parse_literal_string()?,
@@ -2463,9 +2463,9 @@ impl<'a> Parser<'a> {
                     self.prev_token();
                     let type_name = self.parse_object_name()?;
                     if let Some(arguments) = self.parse_optional_type_arguments()? {
-                        Ok(DataType::CustomWithArgs(type_name, arguments))
+                        Ok(DataType::Custom(type_name, arguments))
                     } else {
-                        Ok(DataType::Custom(type_name))
+                        Ok(DataType::Custom(type_name, vec![]))
                     }
                 }
             },
